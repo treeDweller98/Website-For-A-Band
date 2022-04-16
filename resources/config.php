a093fe55c8abc0c1bb24d1a7ca04d06fcaa47937
treeDweller98 <personal.fahim00@gmail.com>
2022-04-16 22:02:03 +0600

    added rendering function

---
+++ resources/config.php
@@ -0,0 +1,34 @@
+<?php
+    // Configs
+    $config = array(
+        "db" => array(
+            "dbname" => "database1",
+            "username" => "dbUser",
+            "password" => "pa$$",
+            "host" => "localhost"
+        ),
+        "urls" => array(
+            "baseUrl" => "http://localhost/website/final/public/"
+        ),
+        "paths" => array(
+            "resources" => "/path/to/resources",
+            "images" => array(
+                "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
+                "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
+            )
+        )
+    );
+
+    
+    // Constants
+    defined("LIBRARY_PATH")
+        or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
+        
+    defined("TEMPLATES_PATH")
+        or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
+    
+    
+    // Error reporting for debugging
+    ini_set("error_reporting", "true");
+    error_reporting(E_ALL);
+?>
--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,15 +1,23 @@
-<?php
-    $output = <<< EOT
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="resources/library/bootstrap/bootstrap.css">
-        <script defer src="resources/library/bootstrap/bootstrap.js"></script>
-        <title>%TITLE%</title>
-    </head>
-    <body>
-    EOT;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/root.css">
+    <script defer src="js/bootstrap.bundle.min.js"></script>
+    <script defer src="js/jquery-3.6.0.min.js"></script>
+
+    <?php 
+        foreach( $stylesheets as $stylesheet ) {
+            echo "<link rel=\"stylesheet\" href=\"css/{$stylesheet}\">";
+        }
+        foreach( $scripts as $script ) {
+            echo "<script defer src=\"js/{$script}\"></script>";
+        }
+    ?>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
