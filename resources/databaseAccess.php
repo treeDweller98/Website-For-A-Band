5de119211bc06679ccee22eecbc8427e5712ad8a
Shafayat1004 <shafayat1004@live.com>
2022-04-20 02:40:20 +0600

    fixed .env acccess

--- resources/databaseAccess.php
+++ resources/databaseAccess.php
@@ -1,8 +1,7 @@
 <?php
     require '../vendor/autoload.php';
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
     $dotenv->safeLoad();
-
 
     $link = mysqli_connect($_ENV["DB_SERVER"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"],$_ENV["DB_NAME"]);
     if($link === false) {
