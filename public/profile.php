010f04c7627bf9f49535ed8f02039d3f1b85dc0c
Sue200 <msuhaila47@gmail.com>
2022-04-19 23:31:53 +0600

    Added query for profile

--- public/profile.php
+++ public/profile.php
@@ -7,15 +7,36 @@
         // Connect to DB
         require_once(realpath(dirname(__FILE__) . "/../resources/databaseAccess.php"));
         
-        // Insert logic here
-        
+        $sql = "SELECT * FROM USER_T WHERE idUser = {$_SESSION['id']}";
+
+    
+        while($rows=$result->fetch_assoc()){ 
+                
+            $fname =  $rows['fname'];
+            $lname=  $rows['lname'];
+            $email =  $rows['email'];
+            $address =  $rows['address'];
+            $zipCode =  $rows['zipCode'];
+            $country =  $rows['country'];
+            $phone =  $rows['phone'];
+            $joined =  $rows['joined'];
+        }
+               
         // Close connection
-        mysqli_stmt_close($stmt);
         mysqli_close($link);
         
         // Render
         $variables = array(
             'title' => "Account | {$_SESSION["username"]}",
+            'fname'  => $fname,
+            'lname'  => $lname,
+            'email'  => $email,
+            'address'  =>$address,
+            'zipCode'  => $zipCode,
+            'country'  => $country,
+            'phone'  => $phone,
+            'joined'  => $joined,
+
         );
         renderLayoutWithContentFile("profile.php", $variables);
 
