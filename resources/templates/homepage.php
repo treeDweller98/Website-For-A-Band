ae16f5613678f9650656c5e56abae7fb47a8de1c
Shafayat1004 <shafayat1004@live.com>
2022-04-22 22:53:57 +0600

    fixed some layout issue

--- resources/templates/homepage.php
+++ resources/templates/homepage.php
@@ -1,12 +1,12 @@
-<aside class="scrollspy-bar">
-    <ul>
-        <li><a href="#mainBanner" class="dot" data-scroll="mainBanner"><span>New Music</span></a></li>
-        <li><a href="#merchSlideshow" class="dot" data-scroll="merchSlideshow"><span>New Merch</span></a></li>
-        <li><a href="#tourDates" class="dot" data-scroll="tourDates"><span>Tour Dates</span></a></li>
-    </ul>
-</aside>
-
 <main>
+    <div class="scrollspy-bar">
+        <ul>
+            <li><a href="#mainBanner" class="dot" data-scroll="mainBanner"><span>New Music</span></a></li>
+            <li><a href="#merchSlideshow" class="dot" data-scroll="merchSlideshow"><span>New Merch</span></a></li>
+            <li><a href="#tourDates" class="dot" data-scroll="tourDates"><span>Tour Dates</span></a></li>
+        </ul>
+    </div>
+
     <section class="sec d-flex flex-lg-row flex-md-column justify-content-center" id="mainBanner">
 
         <div class="d-flex flex-column justify-content-center text-center bg-dark bg-opacity-75 px-5">
@@ -30,90 +30,86 @@
 
     </section>
 
-    <section class="sec d-flex flex-column align-items-center py-5" id="merchSlideshow">
-
-            <h2 class="mb-3">New Merch!</h2>
-
-            <div class="row col-lg-10 justify-content-center">
-                <div id="merchCarouselIndicator" class="carousel slide carousel-dark col-md-10 col-lg-6 h-50" data-bs-ride="carousel">
-                    <div class="carousel-indicators">
-                        <?php
-                            $i = 0;
-                            foreach( $featuredMerch as $slide ) {
-                                $active = ($i == 0) ? "active" : "";
-                                $slideNum = $i+1;
-                                echo <<< HTML
-                                <button type="button" class="{$active}" data-bs-target="#merchCarouselIndicator" data-bs-slide-to="{$i}" aria-label="Slide {$slideNum}"></button>
-                                HTML;
-                                $i++;
-                            }
-                        ?>
-                    </div>
-        
-                    <div class="carousel-inner">
-                        <?php
-                            $i = 0;
-                            foreach( $featuredMerch as $slide ) {
-                                $active = ($i == 0) ? "active" : "";
-                                echo <<< HTML
-                                <div class="carousel-item {$active}" data-bs-interval="2000">
-                                    <img src="{$slide['imageUrl']}" class="d-block card-img" alt="{$slide['name']}">
-        
-                                    <div class="card-img-overlay d-flex justify-content-center align-items-end mb-5">
-                                        <div class="bg-dark bg-opacity-75 p-4 px-5">
-                                            <h5 class="card-title">{$slide['name']}</h5>
-                                            <h6 class="card-subtitle mb-2 text-info">Taka {$slide['price']}/-</h6>
-                                            <p class="card-text">{$slide['description']}</p>
-                                        </div>
-                                        <!-- <a href="{$slide['btnUrl']}" class="btn {$slide['btnStyle']}"></a> -->
-                                    </div>
-                                </div>
-                                HTML;
-                                $i++;
-                            }
-                        ?>
-                    </div>
-        
-                    <button class="carousel-control-prev" type="button" data-bs-target="#merchCarouselIndicator" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#merchCarouselIndicator" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-
-    </section>
-
-    <section class="sec d-flex flex-column align-items-center py-5" id="tourDates">
-
-        <h2 class="mb-3">Upcoming Shows</h2>
-
-        <?php 
-            foreach ( $upcomingConcerts as $concert ) {
-                echo <<< HTML
-                <div class="card bg-dark" style="max-width: 540px;">
-                    <div class="row">
-                        <div class="col-md-4 p-3">
-                            <img src="{$concert['imageUrl']}" class="img-fluid rounded-start" alt="{$concert['name']}">
+    <section class="merch-tour-section">
+        <div class="sec d-flex flex-column align-items-center py-5" id="merchSlideshow">
+                <h2 class="mb-3">New Merch!</h2>
+                <div class="row col-lg-10 justify-content-center carousel-container">
+                    <div id="merchCarouselIndicator" class="carousel slide carousel-bg-dark" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            <?php
+                                $i = 0;
+                                foreach( $featuredMerch as $slide ) {
+                                    $active = ($i == 0) ? "active" : "";
+                                    $slideNum = $i+1;
+                                    echo <<< HTML
+                                    <button type="button" class="{$active}" data-bs-target="#merchCarouselIndicator" data-bs-slide-to="{$i}" aria-label="Slide {$slideNum}"></button>
+                                    HTML;
+                                    $i++;
+                                }
+                            ?>
                         </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">{$concert['name']}</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">{$concert['schedule']}</h6>
-                                <p class="card-text">{$concert['description']}</p>
-                                <div class="bg-transparent border-success">
-                                    <img src="images/icons/location.png" class="d-inline-block align-text-center icon icon-sm" alt="location">
-                                    {$concert['location']}
+        
+                        <div class="carousel-inner">
+                            <?php
+                                $i = 0;
+                                foreach( $featuredMerch as $slide ) {
+                                    $active = ($i == 0) ? "active" : "";
+                                    echo <<< HTML
+                                    <div class="carousel-item {$active}" data-bs-interval="2000">
+                                        <img src="{$slide['imageUrl']}" class="d-block card-img" alt="{$slide['name']}">
+        
+                                        <div class="card-img-overlay d-flex justify-content-center align-items-end mb-5">
+                                            <div class="bg-dark bg-opacity-75 p-4 px-5">
+                                                <h5 class="card-title">{$slide['name']}</h5>
+                                                <h6 class="card-subtitle mb-2 text-info">Taka {$slide['price']}/-</h6>
+                                                <p class="card-text">{$slide['description']}</p>
+                                            </div>
+                                            <!-- <a href="{$slide['btnUrl']}" class="btn {$slide['btnStyle']}"></a> -->
+                                        </div>
+                                    </div>
+                                    HTML;
+                                    $i++;
+                                }
+                            ?>
+                        </div>
+        
+                        <button class="carousel-control-prev" type="button" data-bs-target="#merchCarouselIndicator" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#merchCarouselIndicator" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+        </div>
+        <div class="sec d-flex flex-column align-items-center py-5" id="tourDates">
+            <h2 class="mb-3">Upcoming Shows</h2>
+            <?php
+                foreach ( $upcomingConcerts as $concert ) {
+                    echo <<< HTML
+                    <div class="card bg-dark" style="max-width: 540px;">
+                        <div class="row">
+                            <div class="col-md-4 p-3">
+                                <img src="{$concert['imageUrl']}" class="img-fluid rounded-start" alt="{$concert['name']}">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">{$concert['name']}</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">{$concert['schedule']}</h6>
+                                    <p class="card-text">{$concert['description']}</p>
+                                    <div class="bg-transparent border-success">
+                                        <img src="images/icons/location.png" class="d-inline-block align-text-center icon icon-sm" alt="location">
+                                        {$concert['location']}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                HTML;
-            }
-        ?>
+                    HTML;
+                }
+            ?>
+        </div>
     </section>
 </main>
